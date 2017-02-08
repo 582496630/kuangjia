8377976199c53588827bca7ed10b212036a6e35e
zhouyou <582496630@qq.com>
2017-02-08 19:24:54 +0800

    111

---
+++ Hibernate52-Start/src/com/ddb/Hibernate/start/Users.java
@@ -0,0 +1,32 @@
+package com.ddb.Hibernate.start;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter@Getter
+public class Users {
+
+	private Long id;
+	private String name;
+	private String pass;
+	
+	public Users(){
+	}
+
+	public Users(Long id, String name, String pass) {
+		
+		this.id = id;
+		this.name = name;
+		this.pass = pass;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", name=" + name + ", pass=" + pass + "]";
+	}
+	
+	
+}
