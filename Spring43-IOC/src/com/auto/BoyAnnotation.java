cd275fabbd09c52fdaa6a4ea63bf70a37c6a25da
zhouyou <582496630@qq.com>
2017-02-08 19:22:02 +0800

    hibernate

--- Spring43-IOC/src/com/auto/BoyAnnotation.java
+++ Spring43-IOC/src/com/auto/BoyAnnotation.java
@@ -12,17 +12,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 /*
+@Component("boyAnnotation") 给组件去个名字
 @Autowired 注入属性，可以放在属性或者Setter方法上面   byType自动注入
 @Resource    注入属性，可以放在属性或者Setter方法上面
-                                       属性name解析bean的名字，type解析bean类型，byname自动注入
-             @Resource(name="usaGirlFriend") @Resource(type=UsaGirlFriend.class)
+属性name解析bean的名字，type解析bean类型，byname自动注入
+@Resource(name="usaGirlFriend") @Resource(type=UsaGirlFriend.class)
 
-下面四个注解都可以把类放到容器bean中，，根据这个类是什么功能处于什么层面，来使用哪个注解，不清楚就@Component
-@Component("boyAnnotation") 给组件去个名字
 @Repository   dao层bean(持久化层)
 @Service      Service层bean(服务层)
 @Controller    mvc控制层bean(表现层)
-
 
 @Scope("session") bean的作用域
 singleton   单例，共享bean
