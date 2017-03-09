bdefa2312a6d5038a32e6a571c56f6238ec5c61b
zhouyou <582496630@qq.com>
2017-03-09 10:36:44 +0800

    Struts2Spring4Hibernate4self Signed-off-by: zhouyou <582496630@qq.com>

---
+++ Struts2Spring4Hibernate4self/src/main/java/cn/goodjobs/cms/domain/Users.java
@@ -0,0 +1,63 @@
+package cn.goodjobs.cms.domain;
+
+public class Users {
+	
+	private Long id;
+	
+	private String name;
+	
+	private String pass;
+	
+	private Integer age;
+	
+	private String hobby;
+	
+
+	public Users(){
+		
+	}
+	
+	public Users(Long id){
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+}
